c732a5b713c4e754bf1732faa54b2b2d441ed26e
jessica banka <jessicabanka3081@gmail.com>
2023-01-28 20:58:42 +0530

    bg gradient

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const showDemo = () => {
 
   gsap.utils.toArray('section').forEach((section, index) => {
     const w = section.querySelector('.wrapper');
-    const [x, xEnd] = index % 2 ? ['100%', (w.scrollWidth - section.offsetWidth) * -1] : [w.scrollWidth * -1, 0];
+    const [x, xEnd] = index % 2 ? ['', (w.scrollWidth - section.offsetWidth) * -1] : [w.scrollWidth * -1, 0];
     gsap.fromTo(w, { x }, {
       x: xEnd,
       scrollTrigger: {
